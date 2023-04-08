c97456c155abfcee53e0edd1673b83b40cd43fa0
Andrea Bonatti <andreabonatti92@gmail.com>
2023-04-08 14:18:25 +0200

    Flow Operators Part2 https://www.youtube.com/watch?v=sk3svS_fzZM

--- app/src/main/java/com/example/kotlinflows/MainViewModel.kt
+++ app/src/main/java/com/example/kotlinflows/MainViewModel.kt
@@ -1,9 +1,9 @@
 package com.example.kotlinflows
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
@@ -27,44 +27,93 @@ class MainViewModel : ViewModel() {
         collectFlow()
     }
 
+    // [[1, 2, 3], [1, 2]]
+    // [1, 2, 3, 1, 2] == flattening a list
+
     private fun collectFlow() {
 //            countdownFlow.onEach { time ->
 //                println(time)
 //            }.launchIn(viewModelScope)
 
-        viewModelScope.launch {
+//        val flow1 = flow {
+//            emit(1)
+//            delay(500L)
+//            emit(2)
+//        }
+//
+//        viewModelScope.launch {
+//            flow1.flatMapConcat { value ->
+//                flow {
+//                    emit(value + 1)
+//                    delay(500L)
+//                    emit(value + 2)
+//                }
+//            }.collect { value ->
+//                println("The value is $value")
+//            }
+//        }
 
-            // collect vs collectLatest
-            // if there is a new emission and the previous one is not finished
-            // collectLatest collect only the latest
-            // => a big delay could show us only the latest emission (0)
-            // collectLatest => useful to reflect UI state
-            // where the latest state is the more important
-//            val count = countdownFlow
-            val reduceResult = countdownFlow
-//                .filter { time ->
-//                    time % 2 == 0
-//                }
-//                .map { time ->
-//                    time * time
-//                }
-//                .onEach { time ->
-//                    println(time)
-//                }
-//                .collect { time ->
-//                    println("The current time is $time")
-//                }
-//                .count { time ->
-//                    time % 2 == 0
-//                }
-//                .reduce { accumulator, value ->
+//            // collect vs collectLatest
+//            // if there is a new emission and the previous one is not finished
+//            // collectLatest collect only the latest
+//            // => a big delay could show us only the latest emission (0)
+//            // collectLatest => useful to reflect UI state
+//            // where the latest state is the more important
+////            val count = countdownFlow
+//            val reduceResult = countdownFlow
+////                .filter { time ->
+////                    time % 2 == 0
+////                }
+////                .map { time ->
+////                    time * time
+////                }
+////                .onEach { time ->
+////                    println(time)
+////                }
+////                .collect { time ->
+////                    println("The current time is $time")
+////                }
+////                .count { time ->
+////                    time % 2 == 0
+////                }
+////                .reduce { accumulator, value ->
+////                    accumulator + value
+////                }
+//                .fold(100) { accumulator, value ->
 //                    accumulator + value
 //                }
-                .fold(100) { accumulator, value ->
-                    accumulator + value
-                }
-//            println("The count is $count")
-            println("The reduce result is $reduceResult")
+////            println("The count is $count")
+//            println("The reduce result is $reduceResult")
+//        }
+
+//        val flow1 = (1..5).asFlow()
+//        viewModelScope.launch {
+//            flow1.flatMapConcat { id ->
+//                getRecipeById(id)
+//            }.collect { value ->
+//                println("The value is $value")
+//            }
+//        }
+
+        val flow = flow {
+            delay(250L)
+            emit("Appetizer")
+            delay(1000L)
+            emit("Main dish")
+            delay(100L)
+            emit("Dessert")
+        }
+        viewModelScope.launch {
+            flow.onEach {
+                println("FLOW: $it is delivered")
+            }
+//                .buffer()// The part below runs in a different coroutine
+                .conflate()// The part below runs in a different coroutine, go to the latest emissions
+                .collect {
+                println("FLOW: Now eating $it")
+                delay(1500L)
+                println("FLOW: Finished eating $it")
+            }
         }
     }
 }
